bcef984239130d5d436fce0c4b5ffdbc2f616cd5
RoniBAN <76223243+RoniBAN@users.noreply.github.com>
2021-02-14 18:02:54 -0300

    Add files via upload

---
+++ src/menuvip.js
@@ -0,0 +1,100 @@
+const menu = (prefix) => {
+	return `
+	✎Dono:✞۝ঔৣ𝘽𝙖𝙣✞ O BRABO
+
+             ⚜️ *MENU VIP* ⚜️
+
+
+╔════════════════════
+║   ⚜️_Comandos abaixo_⚜️
+╠════════════════════
+║${prefix}welcome [0/1] 
+║${prefix}bemvindo [0/1]
+║${prefix}boasvindas [0/1]
+╠════════════════════
+║1 Para ativar o modo em que a
+║Zero Two de as boas vindas aos 
+║novos membros 
+║0 Para desativar o modo
+╠════════════════════
+║!add 55xxx
+║!adicionar 55xxx
+╠════════════════════
+║USO : Apenas digite o comando
+║mais o número desejado
+║FUNÇÃO : adicionar membros
+╠═══════════════════
+║!kick @membro
+║!ban @membro
+║!remover @membro
+╠════════════════════
+║USO : Apenas digite o comando 
+║mais o @ do membro 
+║
+║FUNÇÃO : remover membros
+╠═══════════════════
+║!promover @membro
+╠════════════════════
+║USO : Apenas digite o comando 
+║mais o @ desejado
+║
+║FUNÇÃO :  promover membros á 
+║ADM
+╠════════════════════
+║!demitir @adm
+╠═══════════════════
+║USO : Apenas digite o comando
+║Com o @ Do adm desejado
+║FUNÇÃO: rebaixar a membro
+╠═══════════════════
+║!grupo(fechar/abrir)
+║!abrirgp (fechargp)
+╠═══════════════════
+║ USO: Apenas digite o comando
+║exemplo (!grupo/abrir) 
+║FUNÇÃO: Abrir e fechar o grupo
+╠═══════════════════
+║  !tagall 
+║  !tagall2
+║  !tagall3
+║  !tagall4
+║  !tagall5
+╠════════════════════
+║USO : Apenas digite o comando
+║
+║FUNÇÃO : Marcar todos os 
+║membros 
+╠═══════════════════
+║  !level  [0/1]
+╠═══════════════════
+║USO: Apenas digite o comando 
+║
+║FUNÇÃO: ativa o modo de nível
+╠═══════════════════
+║ !setprefix (símbolo desejado)
+╠═══════════════════
+║USO: Apenas digite o comando 
+║com o símbolo q vc deseja ser o 
+║prefixo exemplo: !setprefix /
+║
+║FUNÇÃO: muda o prefixo 
+╠═══════════════════
+║ !hentai
+║ !hentai2
+║ !hentai3
+╠═══════════════════
+║USO: Apenas digite o comando
+║
+║FUNÇÃO: manda imagens hentai
+╠═══════════════════
+║ !block @individuo
+╠═══════════════════
+║USO: Apenas digite o comando
+║
+║FUNÇÃO: bloqueia a pessoa 
+║marcada
+╚═════════════════════
+`
+}
+
+exports.menu = menu
