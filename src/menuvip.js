228e7387098515a7013a3a724d3a76ec429a5f1c
RoniBAN <76223243+RoniBAN@users.noreply.github.com>
2021-02-16 19:54:25 -0300

    Add files via upload

---
+++ src/menuvip.js
@@ -0,0 +1,108 @@
+const menuvip = (prefix) => {
+	return `
+	✎Dono:✞۝ঔৣ𝘽𝙖𝙣✞ O BRABO
+
+             ⚜️ *MENU VIP* ⚜️
+
+
+╔════════════════════
+║   ⚜️_Comandos abaixo_⚜️
+╠════════════════════
+║ ${prefix}welcome [0/1] 
+║ ${prefix}bemvindo [0/1]
+║ ${prefix}boasvindas [0/1]
+╠════════════════════
+║1 Para ativar o modo em que a
+║Zero Two de as boas vindas aos 
+║novos membros 
+║0 Para desativar o modo
+╠════════════════════
+║ ${prefix}add 55xxx
+║ ${prefix}adicionar 55xxx
+╠════════════════════
+║USO : Apenas digite o comando
+║mais o número desejado
+║FUNÇÃO : adicionar membros
+╠═══════════════════
+║ ${prefix}kick @membro
+║ ${prefix}ban @membro
+║ ${prefix}remover @membro
+╠════════════════════
+║USO : Apenas digite o comando 
+║mais o @ do membro 
+║
+║FUNÇÃO : remover membros
+╠═══════════════════
+║ ${prefix}promover @membro
+╠════════════════════
+║USO : Apenas digite o comando 
+║mais o @ desejado
+║
+║FUNÇÃO :  promover membros á 
+║ADM
+╠════════════════════
+║ ${prefix}demitir @adm
+╠═══════════════════
+║USO : Apenas digite o comando
+║Com o @ Do adm desejado
+║FUNÇÃO: rebaixar a membro
+╠═══════════════════
+║ ${prefix}grupo(fechar/abrir)
+║ ${prefix}abrirgp (fechargp)
+╠═══════════════════
+║ USO: Apenas digite o comando
+║exemplo (!grupo/abrir) 
+║FUNÇÃO: Abrir e fechar o grupo
+╠═══════════════════
+║ ${prefix}tagall 
+║ ${prefix}tagall2
+║ ${prefix}tagall3
+║ ${prefix}tagall4
+║ ${prefix}tagall5
+╠════════════════════
+║USO : Apenas digite o comando
+║
+║FUNÇÃO : Marcar todos os 
+║membros 
+╠═══════════════════
+║ ${prefix}level  [0/1]
+╠═══════════════════
+║USO: Apenas digite o comando 
+║
+║FUNÇÃO: ativa o modo de nível
+╠═══════════════════
+║ ${prefix}linkgroup
+║ ${prefix}linkgrupo
+║ ${prefix}linkgc
+╠═══════════════════
+║USO: Apenas digite o comando 
+║
+║FUNÇÃO: manda o link do gp
+╠═══════════════════
+║ ${prefix}setprefix (símbolo desejado)
+╠═══════════════════
+║USO: Apenas digite o comando 
+║com o símbolo q vc deseja ser o 
+║prefixo exemplo: !setprefix /
+║
+║FUNÇÃO: muda o prefixo 
+╠═══════════════════
+║ ${prefix}hentai
+║ ${prefix}hentai2
+║ ${prefix}hentai3
+╠═══════════════════
+║USO: Apenas digite o comando
+║
+║FUNÇÃO: manda imagens hentai
+╠═══════════════════
+║ ${prefix}block @individuo
+╠═══════════════════
+║USO: Apenas digite o comando
+║
+║FUNÇÃO: bloqueia a pessoa 
+║marcada
+╚═════════════════════
+`
+}
+
+exports.menuvip = menuvip
