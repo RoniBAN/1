3eb8dd3226db521dba5d394eff94426539d1508c
RoniBAN <76223243+RoniBAN@users.noreply.github.com>
2021-02-16 19:35:55 -0300

    Add files via upload

---
+++ src/download.js
@@ -0,0 +1,33 @@
+const download = (prefix) => {
+	return `
+✎Dono:✞۝ঔৣ𝘽𝙖𝙣✞ O BRABO
+
+             ⚜️ *DOWNLOAD* ⚜️
+
+
+╔════════════════════
+║  ⚜️ _Comandos abaixo_ ⚜️
+╠════════════════════
+║ ${prefix}ytmp4  LINK
+║ ${prefix}baixar   LINK
+╠════════════════════
+║USO: use o comando com um link
+║do youtube
+║
+║Função: Baixa videos do YouTube
+║E Manda pronto pra vc,pode 
+║demorar um pouco 
+╠════════════════════
+║ ${prefix}play (nome da música)
+║ ${prefix}tocar (...)
+╠════════════════════
+║USO: use o comando com o nome ║da musica que vc quer baixar
+║
+║Função: Baixa videos do YouTube
+║E Manda pronto pra vc,pode 
+║demorar um pouco 
+╚═════════════════════
+`
+}
+
+exports.download = download
---
+++ src/midia.js
@@ -0,0 +1,111 @@
+const midia = (prefix) => {
+	return `
+✎Dono:✞۝ঔৣ𝘽𝙖𝙣✞ O BRABO
+
+             ⚜️ *MÍDIA* ⚜️
+
+
+╔════════════════════
+║  ⚜️ _Comandos abaixo_ ⚜️
+╠═══════════════════
+║ ${prefix}canal
+╠═══════════════════
+║USO:Apenas envie o comandos
+║
+║FUNÇÃO: Envia uma imagem e 
+║link do canal do nosso adm pika 
+║das galáxias satã
+╠═══════════════════
+║   ${prefix}sticker
+║   ${prefix}stiker
+║   ${prefix}figu
+║   ${prefix}figurinha
+╠═══════════════════
+║USO :  Enviei a imagem deseja 
+║como o comando na descrição. 
+║
+║FUNÇÃO : faz figurinha com a 
+║foto/gif máximo de 7segundos
+╠═══════════════════
+║ ${prefix}toimg marca figu
+║ ${prefix}converter marca figu
+╠════════════════════
+║USO :  Apenas digite o comando 
+║marcando a figu
+║
+║FUNÇÃO : converter figu em foto.
+╠═══════════════════
+║ ${prefix}tts seu txt
+║ ${prefix}gtts seu txt
+║ ${prefix}audio seu txt
+╠════════════════════
+║USO : Apenas digite o comando 
+║mais o texto desejado com o 
+║idioma que vc quer
+║exemplo: !gtts pt oi família
+║
+║FUNÇÃO :  Converter text em 
+║áudio. Para saber quais idiomas 
+║ela faz digite !idiomas
+╠════════════════════
+║ ${prefix}txtdafoto
+║ ${prefix}txt 
+║ ${prefix}ocr
+╠═══════════════════
+║USO :  Apenas digite o comando
+║na descrição da imagem 
+║desejada
+║
+║FUNÇÃO:  pegar txt de foto
+╠═══════════════════
+║ ${prefix}wait marca foto
+╠═══════════════════
+║USO:  Apenas digite o comando 
+║numa foto
+║FUNÇÃO: Pesquisar por anime de 
+║fotos
+╠═══════════════════
+║ ${prefix}cachorro
+║ ${prefix}auau
+║ ${prefix}dog
+╠═══════════════════
+║USO : Apenas digite o comando
+║
+║FUNÇÃO : ver fotos de dogs
+║aleatórios 
+╠═══════════════════
+║ ${prefix}Pokemon 
+╠═══════════════════
+║USO: Apenas digite o comando
+║
+║FUNÇÃO: Pokémon aleatório 
+╠═══════════════════
+║ ${prefix}adm
+╠═════════════════════
+║USO : Apenas digite o comando
+║
+║FUNÇÃO : Envia uma ft de 
+║hentai,com uma brincadeira 
+║falando do adm ban
+╠═══════════════════
+║ ${prefix}fbi
+╠═════════════════════
+║USO : Apenas digite o comando
+║Quando alguem usar o !loli
+║
+║FUNÇÃO : Envia uma imagem  
+║Relacionada ao meme da loli
+╠═══════════════════
+║ ${prefix}fbi1
+╠═════════════════════
+║USO : Apenas digite o comando
+║
+║FUNÇÃO : msm coisa do !fbi
+║soq com imagem e legenda 
+║diferentes
+╚═════════════════════
+
+`
+}
+
+exports.midia = midia
